1085def75bfa20a018a9b3d1c8e53b4b1076c95f
saad <saad.khan@airwalkconsulting.com>
2019-12-18 16:24:43 +0000

    added region in py script

--- tests/post_deploy_test.py
+++ tests/post_deploy_test.py
@@ -21,7 +21,7 @@ requiredNamed.add_argument('--secret_key', help="example: secret_key=bdskjdkdkad
 requiredNamed.add_argument('--instance_id', help="example: instance_id=i-0ds9f9sf9", required=True)
 args = parser.parse_args()
 if __name__ == "__main__":
-    session = Session(aws_access_key_id=args.access_key, aws_secret_access_key=args.secret_key)
+    session = Session(aws_access_key_id=args.access_key, aws_secret_access_key=args.secret_key, region_name='eu-west-1')
     if verify_ec2_status(args.instance_id):
         sys.exit(0)
     else:
